883940b0c07f833967d301ad662e7b37439ba1aa
mitanshi <mitanshi1995.mr@gmail.com>
2017-06-16 16:30:42 +0530

    Till tutorial 19. It's fun!

--- music/views.py
+++ music/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
 from .models import Album
+from django.http import Http404
+from django.shortcuts import render
 from django.template import loader
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_albums' : all_albums,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'all_albums' : all_albums,}
+    return render(request, 'music/index.html', context)
+
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id:"+str(album_id)+"</h2>")
+    try:
+        album = Album.objects.get(id=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Oops Album Does not Exist!")
+    return render(request, 'music/detail.html', {'album' : album})
